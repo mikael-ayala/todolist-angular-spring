56bf59c87427a25f265adc64b9de5084baea5849
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-01-06 12:43:09 -0300

    initial commit

---
+++ todolist-spring/src/main/java/todolistspring/TodolistSpringApplication.java
@@ -0,0 +1,13 @@
+package todolistspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistSpringApplication.class, args);
+	}
+
+}
---
+++ todolist-spring/src/test/java/todolistspring/TodolistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package todolistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
