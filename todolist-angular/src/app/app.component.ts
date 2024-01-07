d8eb6439572f9d99db0f45e3874e8c0cfca8a862
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-01-07 16:52:43 -0300

    add angular

---
+++ todolist-angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,
+            RouterOutlet,
+            HeaderComponent
+          ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {}
