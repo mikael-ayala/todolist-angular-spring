7ac10f22027e77b667be5ccf8306aaef3a4d8384
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-01-07 18:10:18 -0300

    add angular standalone=false

--- todolist-angular/src/app/app.component.ts
+++ todolist-angular/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule,
-            RouterOutlet,
-            HeaderComponent
-          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'todolist-angular';
+}
