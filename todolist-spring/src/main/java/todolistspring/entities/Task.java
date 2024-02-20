fc43575875d19ade902bb8c7548c713314e72bd7
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-02-20 14:09:19 -0300

    config test properties and add columnDefinition on Task

--- todolist-spring/src/main/java/todolistspring/entities/Task.java
+++ todolist-spring/src/main/java/todolistspring/entities/Task.java
@@ -17,6 +17,8 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
     private boolean isCompleted;
 
